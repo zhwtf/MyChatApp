7492bc9cfc86d47e830d1c7910b45dcd10aee469
haozheng <hza89@sfu.ca>
2018-01-26 21:55:38 -0800

    open photo picker

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -54,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int RC_SIGN_IN = 1;
 
+    private static final int RC_PHOTO_PICKER = 2;
+
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
     private ProgressBar mProgressBar;
@@ -105,6 +107,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO: Fire an intent to show an image picker
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+
             }
         });
 
@@ -154,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
 //            public void onCancelled(DatabaseError databaseError) {}
 //        };
 
-        mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
+        //mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
 
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
